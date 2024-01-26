1a0656ad14a6832880aaa6449fe64a52668154d2
Teglas Adam <ad.teglas@gmail.com>
2024-01-26 08:23:24 +0100

    Card components done, basic css done, font imported, README.md updated

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 Hi. I'm Adam.
 
-This is my React-Card Homework. 
+This is my React-Card Homework.
 
 Just a simple background changer for practice useEffect and useState hooks.
+
+Used colors:
+-body background: rgba(51, 51, 51, 0.3)
+
+-card standard background: rgb(90, 118, 130, 0.4)
+
+-font standard color: rgb(0, 0, 0)
+
+-"Magic" text font color:
+    linear-gradient(
+    90deg,
+    rgb(219, 191, 28) 0%,
+    rgba(227, 89, 150, 1) 50%,
+    rgb(11, 109, 129) 100%)
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,5 +2,15 @@ import "./Card.css";
 import { useState, useEffect } from "react";
 
 export default function Card() {
-  return (<div>Card</div>);
+  return (
+    <div className="container">
+      <div className="card">
+        <h1 className="cardTitle">Not so <span className="rainbowColor">Magic</span> Card</h1>
+        <p className="cardParag">
+          If you drag the mouse over the card, its color will change.
+        </p>
+        <button className="cardButton">Useless Element</button>
+      </div>
+    </div>
+  );
 }
