39020c2e87a53635b3300656192ae2ea5895968b
Teglas Adam <ad.teglas@gmail.com>
2024-01-26 10:20:18 +0100

    homework done, mouseover working, README updated

--- README.md
+++ README.md
@@ -5,11 +5,13 @@ This is my React-Card Homework.
 Just a simple background changer for practice useEffect and useState hooks.
 
 Used colors:
--body background: rgba(51, 51, 51, 0.3)
+-body background: rgba(206, 83, 83, 0.3)
 
 -card standard background: rgb(90, 118, 130, 0.4)
+-card recolored color: rgb(50, 50, 50, 0.8)
 
--font standard color: rgb(0, 0, 0)
+-font standard color: black
+-font recolored color: white
 
 -"Magic" text font color:
     linear-gradient(
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,33 @@ import "./Card.css";
 import { useState, useEffect } from "react";
 
 export default function Card() {
+  //A háttér és a szöveg színeinek változói
+  const [bgColor, setBgColor] = useState("rgb(90, 118, 130, 0.4)");
+  const [fontColor, setFontColor] = useState("black");
+
+  //Az a függvény, amelyik átszínez.
+  function changedColors() {
+    setBgColor("rgb(50, 50, 50, 0.8)");
+    setFontColor("white");
+  }
+
+  //Az a függvény amelyik visszaállítja az eredeti színeket.
+  function standardColors() {
+    setBgColor("rgb(90, 118, 130, 0.4)");
+    setFontColor("black");
+  }
+
   return (
     <div className="container">
-      <div className="card">
-        <h1 className="cardTitle">Not so <span className="rainbowColor">Magic</span> Card</h1>
+      <div
+        className="card"
+        style={{ backgroundColor: `${bgColor}`, color: `${fontColor}` }}
+        onMouseEnter={changedColors}
+        onMouseLeave={standardColors}
+      >
+        <h1 className="cardTitle">
+          Not so <span className="rainbowColor">Magic</span> Card
+        </h1>
         <p className="cardParag">
           If you drag the mouse over the card, its color will change.
         </p>
