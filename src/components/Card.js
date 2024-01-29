71e0dc12af4bdf39e31cf6bf8af7e5636b1ac254
Teglas Adam <ad.teglas@gmail.com>
2024-01-29 09:44:28 +0100

    usable Button added for colorchange

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,25 @@
 import "./Card.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export default function Card() {
   //A háttér és a szöveg színeinek változói
   const [bgColor, setBgColor] = useState("rgb(90, 118, 130, 0.4)");
   const [fontColor, setFontColor] = useState("black");
+  const [magicColor0, setMagicColor0] = useState("rgb(219, 191, 28)");
+  const [magicColor50, setMagicColor50] = useState("rgba(227, 89, 150, 1)");
+  const [magicColor100, setMagicColor100] = useState("rgb(11, 109, 129)");
+
+  /* 
+  Amikor JavaScript kódot szeretnék beilleszteni akkor a backticket kell használjam a sima aposztrofok helyett.
+  példa1: backgroundImage: `linear-gradient(90deg, ${magicColor0} 0%, ${magicColor50} 50%, ${magicColor100} 100%)`
+  példa2: backgroundImage: 'linear-gradient(90deg, rgb(219, 191, 28) 0%, rgba(227, 89, 150, 1) 50%, rgb(11, 109, 129) 100%)'
+  Példa1ben backtickel kezdek és akkor betudok illeszteni változókat példa2ben pedig sima aposztrofokkal írom le a css szabályt, mert String típusú teljes mértékben. */
+  //!!!Probléma volt, hogy backgroundColort próbáltam meg használni, pedig a linear-gradienthez backgroundImage kell!!!
+  const style = {
+    backgroundImage: `linear-gradient(90deg, ${magicColor0} 0%, ${magicColor50} 50%, ${magicColor100} 100%)`,
+    WebkitBackgroundClip: "text",
+    WebkitTextFillColor: "transparent",
+  };
 
   //Az a függvény, amelyik átszínez.
   function changedColors() {
@@ -18,6 +33,20 @@ export default function Card() {
     setFontColor("black");
   }
 
+  //Randomszám generáló függvény random színhez (0-255).
+  const randomMath = () => {
+    return Math.round(Math.random() * 255);
+  };
+
+  //Az a függvény amelyik változtatja gomnyomásra a "MAGIC" szöveg színeit.
+  function changedMagicColors() {
+    setMagicColor0(`rgb(${randomMath()}, ${randomMath()}, ${randomMath()})`);
+    setMagicColor50(
+      `rgba(${randomMath()}, ${randomMath()}, ${randomMath()}, 1)`
+    );
+    setMagicColor100(`rgb(${randomMath()}, ${randomMath()}, ${randomMath()})`);
+  }
+
   return (
     <div className="container">
       <div
@@ -27,12 +56,18 @@ export default function Card() {
         onMouseLeave={standardColors}
       >
         <h1 className="cardTitle">
-          Not so <span className="rainbowColor">Magic</span> Card
+          Not so{" "}
+          <span className="rainbowColor" style={style}>
+            Magic
+          </span>{" "}
+          Card
         </h1>
         <p className="cardParag">
           If you drag the mouse over the card, its color will change.
         </p>
-        <button className="cardButton">Useless Element</button>
+        <button className="cardButton" onClick={changedMagicColors}>
+          Press the Magical Element
+        </button>
       </div>
     </div>
   );
